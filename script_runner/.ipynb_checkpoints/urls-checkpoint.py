e39c502a20bf086fb105cb4322aff51a4e9d2974
jovalni <138931550+jovalni@users.noreply.github.com>
2024-05-23 16:50:48 +0300

    Delete script_runner directory

--- script_runner/.ipynb_checkpoints/urls-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('run-script/', views.run_script, name='run_script'), # POST endpoint for running scripts
-    path('leaflet-map/', views.leaflet_map, name='leaflet_map'),
-    path('process-polygon/', views.process_polygon, name='process_polygon'),
-]
